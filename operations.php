f1f5f912ac580c9e4e4a4aa31a7d2c5f9bbb4daa
HanumantU <hanumant.uttekar@gmail.com>
2018-12-03 16:07:21 +0530

    Task2 Completed : Added Function for Addition of Numbers.

--- operations.php
+++ operations.php
@@ -2,6 +2,9 @@
 
 class Operations {
 		
+	/* Function used to do summation of all the numbers passed to it in arguments.
+	* @param $parameters String
+	*/
 	public function sum($parameters) {
 		$sum = 0;
 		if(!empty($parameters)) {
@@ -13,6 +16,17 @@ class Operations {
 		echo "Sum is : ".$sum;
 	}
 	
-	
-	
+	/* Function used to do Addition of all the numbers passed to it in arguments.
+	* @param $parameters String
+	*/
+	public function add($parameters) {
+		$addition = 0;
+		if(!empty($parameters)) {
+			$values = explode(',', $parameters);
+			foreach($values as $each_value) {
+				$addition += $each_value;
+			}
+		}
+		echo "Addition is : ".$addition;
+	}
 }
